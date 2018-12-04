15d76c643fbec6ab429e7f23a3148a3899116f16
RyannDev <dev@folm.io>
2018-12-04 14:27:35 +0300

    chainparams main rewards

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2014-2016 The Dash developers
-// Copyright (c) 2015-2017 The PIVX developers// Copyright (c) 2017-2018 The ALQO & Bitfineon developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017-2018 The cosmitto & Bitfineon developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -12,7 +13,7 @@
 #include "spork.h"
 
 //
-// Bootup the Masternode, look for a 10000 ALQO input and register on the network
+// Bootup the Masternode, look for a 10000 cosmitto input and register on the network
 //
 void CActiveMasternode::ManageStatus()
 {
--- src/blocksignature.h
+++ src/blocksignature.h
@@ -1,9 +1,10 @@
-// Copyright (c) 2015-2017 The PIVX developers// Copyright (c) 2017-2018 The ALQO & Bitfineon developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017-2018 The COSMITTO & Bitfineon developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef ALQO_BLOCKSIGNATURE_H
-#define ALQO_BLOCKSIGNATURE_H
+#ifndef COSMITTO_BLOCKSIGNATURE_H
+#define COSMITTO_BLOCKSIGNATURE_H
 
 #include "key.h"
 #include "primitives/block.h"
@@ -13,4 +14,4 @@ bool SignBlockWithKey(CBlock& block, const CKey& key);
 bool SignBlock(CBlock& block, const CKeyStore& keystore);
 bool CheckBlockSignature(const CBlock& block);
 
-#endif //ALQO_BLOCKSIGNATURE_H
+#endif //COSMITTO_BLOCKSIGNATURE_H
